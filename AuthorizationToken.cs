f7d543842d224caf8f3fe278b3f29e73a444c87a
JohnCardenas <Cardenas.John.J@gmail.com>
2015-04-16 18:43:41 -0700

    Plugin ribbon buttons and icons added

---
+++ AuthorizationToken.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiresonPortalAPI
+{
+    public class AuthorizationException : Exception
+    {
+        public AuthorizationException() { }
+        public AuthorizationException(string message) : base(message) { }
+        public AuthorizationException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class AuthorizationToken
+    {
+        private string _sUserName;
+        private string _sDomain;
+        private string _sLanguageCode;
+        private string _sAuthToken;
+
+        public string UserName     { get { return _sUserName;     } }
+        public string Domain       { get { return _sDomain;       } }
+        public string LanguageCode { get { return _sLanguageCode; } }
+        public string AuthToken    { get { return _sAuthToken;    } }
+
+        private AuthorizationToken(string userName, string domain, string languageCode, string authToken)
+        {
+            _sUserName     = userName;
+            _sDomain       = domain;
+            _sLanguageCode = languageCode;
+            _sAuthToken    = authToken;
+        }
+
+        public static AuthorizationToken GetToken(string userName, string domain, string password, string languageCode)
+        {
+            return new AuthorizationToken(userName, domain, languageCode, "");
+        }
+    }
+}
