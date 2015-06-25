e3ed0386e4e74876767b3b8cb9f63d32727bafca
JohnCardenas <Cardenas.John.J@gmail.com>
2015-06-25 15:32:52 -0700

    Added default templates for Incidents and Service Requests

---
+++ TemplateConstants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiresonPortalAPI
+{
+    public static class TemplateConstants
+    {
+        /// <summary>
+        /// Built-in templates for Incidents
+        /// </summary>
+        public static class Incident
+        {
+            /// <summary>
+            /// Default Incident Template (DefaultIncidentTemplate)
+            /// Use when you want to open an incident for which you do not have a specific template
+            /// </summary>
+            public static const Guid Default = new Guid("{a77bb0c9-e201-dd93-230c-799a66d9e8fa}");
+        }
+
+        /// <summary>
+        /// Built-in templates for Service Requests
+        /// </summary>
+        public static class ServiceRequest
+        {
+            /// <summary>
+            /// Default Service Request (ServiceManager.ServiceRequest.Library.Template.DefaultServiceRequest)
+            /// Default Service Request
+            /// </summary>
+            public static const Guid Default = new Guid("{03bc9162-041f-c987-8ce4-a5547cd9ca04}");
+        }
+    }
+}
