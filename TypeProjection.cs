0f09ed842ed61b2876baaeec8cedd27681a87168
JohnCardenas <Cardenas.John.J@gmail.com>
2015-07-14 11:17:25 -0700

    Add TypeProjection and User classes to CPAPI

---
+++ TypeProjection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+
+namespace CiresonPortalAPI
+{
+    public class TypeProjection
+    {
+        const string ENDPOINT = "/api/V3/Projection/GetProjectionByCriteria";
+
+    }
+}
