0749f9ccd41ce749af94e371f0b72152c35640a2
JohnCardenas <Cardenas.John.J@gmail.com>
2015-07-20 11:46:15 -0700

    Add GetProperty<T>() for retrieving properties on dynamic objects safely

--- CiresonPortalAPI/DynamicObjectHelpers.cs
+++ CiresonPortalAPI/DynamicObjectHelpers.cs
@@ -19,5 +19,23 @@ namespace CiresonPortalAPI
         {
             return ((IDictionary<string, Object>) expando).ContainsKey(key);
         }
+
+        
+        /// <summary>
+        /// Returns a dynamic object property if it is set, otherwise it returns the default value of the type.
+        /// </summary>
+        /// <typeparam name="T">Type of the property to retrieve</typeparam>
+        /// <param name="expando">Dynamic object</param>
+        /// <param name="key">Property to retrieve</param>
+        /// <returns></returns>
+        public static T GetProperty<T>(ExpandoObject expando, string key)
+        {
+            if (!HasProperty(expando, key))
+                return default(T);
+
+            var exp = expando as IDictionary<string, Object>;
+
+            return (T)Convert.ChangeType(exp[key], typeof(T));
+        }
     }
 }
