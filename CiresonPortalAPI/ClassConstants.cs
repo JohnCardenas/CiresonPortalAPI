ef5fc52aea972d0e9acfac063dfdbe8dcc760778
JohnCardenas <Cardenas.John.J@gmail.com>
2015-07-24 11:34:08 -0700

    Add Cireson.AssetManagement classes

--- CiresonPortalAPI/ClassConstants.cs
+++ CiresonPortalAPI/ClassConstants.cs
@@ -12,14 +12,49 @@ namespace CiresonPortalAPI
     public static class ClassConstants
     {
         /// <summary>
+        /// Catalog Item (Cireson.AssetManagement.CatalogItem)
+        /// </summary>
+        public static Guid CatalogItem { get { return new Guid("{c0c58e7f-7865-55cc-4600-753305b9be64}"); } }
+        
+        /// <summary>
         /// Change Request (System.WorkItem.ChangeRequest)
         /// </summary>
         public static Guid ChangeRequest { get { return new Guid("{e6c9cf6e-d7fe-1b5d-216c-c3f5d2c7670c}"); } }
-        
+
+        /// <summary>
+        /// Cost Center (Cireson.AssetManagement.CostCenter)
+        /// </summary>
+        public static Guid CostCenter { get { return new Guid("{128bdb2d-f5bd-f8b6-440e-e3f7d8ab4858}"); } }
+
+        /// <summary>
+        /// Hardware Asset (Cireson.AssetManagement.HardwareAsset)
+        /// </summary>
+        public static Guid HardwareAsset { get { return new Guid("{c0c58e7f-7865-55cc-4600-753305b9be64}"); } }
+
         /// <summary>
         /// Incident (System.WorkItem.Incident)
         /// </summary>
         public static Guid Incident { get { return new Guid("{a604b942-4c7b-2fb2-28dc-61dc6f465c68}"); } }
+
+        /// <summary>
+        /// Invoice (Cireson.AssetManagement.Invoice)
+        /// </summary>
+        public static Guid Invoice { get { return new Guid("{e57c1c12-16cf-3e2d-576e-c9be562a1a37}"); } }
+
+        /// <summary>
+        /// Lease (Cireson.AssetManagement.Lease)
+        /// </summary>
+        public static Guid Lease { get { return new Guid("{da47b130-6bf6-10eb-b8a0-a9288b729160}"); } }
+
+        /// <summary>
+        /// License (Cireson.AssetManagement.License)
+        /// </summary>
+        public static Guid License { get { return new Guid("{a3ad0993-def0-e2ff-dbcf-9ca04040a219}"); } }
+
+        /// <summary>
+        /// Location (Cireson.AssetManagement.Location)
+        /// </summary>
+        public static Guid Location { get { return new Guid("{b1ae24b1-f520-4960-55a2-62029b1ea3f0}"); } }
 
         /// <summary>
         /// Manual Activity (System.WorkItem.Activity.ManualActivity)
@@ -27,10 +62,25 @@ namespace CiresonPortalAPI
         public static Guid ManualActivity { get { return new Guid("{7ac62bd4-8fce-a150-3b40-16a39a61383d}"); } }
 
         /// <summary>
+        /// Organization (Cireson.AssetManagement.Organization)
+        /// </summary>
+        public static Guid Organization { get { return new Guid("{ed0d8659-fba9-6e08-c213-5cd88f5480a8}"); } }
+
+        /// <summary>
         /// Problem (System.WorkItem.Problem)
         /// </summary>
         public static Guid Problem { get { return new Guid("{422afc88-5eff-f4c5-f8f6-e01038cde67f}"); } }
-        
+
+        /// <summary>
+        /// Purchase (Cireson.AssetManagement.Purchase)
+        /// </summary>
+        public static Guid Purchase { get { return new Guid("{001556ed-3ad5-5640-fee8-beb748da9e03}"); } }
+
+        /// <summary>
+        /// Purchase Order (Cireson.AssetManagement.PurchaseOrder)
+        /// </summary>
+        public static Guid PurchaseOrder { get { return new Guid("{2afe355c-24a7-b20f-36e3-253b7249818d}"); } }
+
         /// <summary>
         /// Release Record (System.WorkItem.ReleaseRecord)
         /// </summary>
@@ -45,5 +95,30 @@ namespace CiresonPortalAPI
         /// Service Request (System.WorkItem.ServiceRequest)
         /// </summary>
         public static Guid ServiceRequest { get { return new Guid("{04b69835-6343-4de2-4b19-6be08c612989}"); } }
+
+        /// <summary>
+        /// Software Asset (Cireson.AssetManagement.SoftwareAsset)
+        /// </summary>
+        public static Guid SoftwareAsset { get { return new Guid("{81e3da4f-e41c-311e-5b05-3ca779d030db}"); } }
+
+        /// <summary>
+        /// Standard (Cireson.AssetManagement.Standard)
+        /// </summary>
+        public static Guid Standard { get { return new Guid("{f096d2f1-22cc-6b40-3b49-a615a5946ce5}"); } }
+
+        /// <summary>
+        /// Subnet (Cireson.AssetManagement.Subnet)
+        /// </summary>
+        public static Guid Subnet { get { return new Guid("{1a7a71a6-de25-61c4-a9c6-a3420c5a8564}"); } }
+
+        /// <summary>
+        /// Support Contract (Cireson.AssetManagement.SupportContract)
+        /// </summary>
+        public static Guid SupportContract { get { return new Guid("{b2c105d4-d8c7-b57c-fe3d-205d47e07141}"); } }
+
+        /// <summary>
+        /// Vendor (Cireson.AssetManagement.Vendor)
+        /// </summary>
+        public static Guid Vendor { get { return new Guid("{f26c94f2-1045-3d60-4c1f-59b8cbfe9931}"); } }
     }
 }
