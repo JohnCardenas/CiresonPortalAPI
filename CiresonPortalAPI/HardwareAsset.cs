cfa49c61896f7c3b6070157f5fb7aae8cb803969
JohnCardenas <Cardenas.John.J@gmail.com>
2015-08-03 14:58:32 -0700

    Add PropertyPathHelper and HardwareAsset classes

---
+++ CiresonPortalAPI/HardwareAsset.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace CiresonPortalAPI
+{
+    public static partial class HardwareAssetController
+    {
+        /// <summary>
+        /// Convenience method that returns a HardwareAsset by its Asset Tag
+        /// </summary>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="assetTag">Asset Tag to retrieve</param>
+        /// <returns>HardwareAsset</returns>
+        public static async Task<HardwareAsset> GetAssetByAssetTag(AuthorizationToken authToken, string assetTag)
+        {
+            QueryCriteriaExpression expression = new QueryCriteriaExpression();
+            expression.PropertyName = (new PropertyPathHelper(ClassConstants.HardwareAsset, "AssetTag")).ToString();
+            expression.PropertyType = QueryCriteriaPropertyType.Property;
+            expression.Operator = QueryCriteriaExpressionOperator.Equal;
+            expression.Value = assetTag;
+
+            QueryCriteria criteria = new QueryCriteria(TypeProjectionConstants.HardwareAsset);
+            criteria.GroupingOperator = QueryCriteriaGroupingOperator.SimpleExpression;
+            criteria.Expressions.Add(expression);
+
+            List<TypeProjection> projectionList = await TypeProjectionController.GetProjectionByCriteria(authToken, criteria);
+
+            if (projectionList.Count == 0)
+                return null;
+
+            return new HardwareAsset(projectionList[0]);
+        }
+    }
+
+
+    public class HardwareAsset
+    {
+
+    }
+}
---
+++ CiresonPortalAPI/PropertyPathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiresonPortalAPI
+{
+    /// <summary>
+    /// Helper class to construct the path to a property in the format $Context/Property[Type]/PropertyName$
+    /// </summary>
+    internal class PropertyPathHelper
+    {
+        private Guid   _oObjectClass;
+        private string _sPropertyName;
+
+        /// <summary>
+        /// Constructs a new PropertyPathHelper
+        /// </summary>
+        /// <param name="objectClass">GUID of the object class</param>
+        /// <param name="propertyName">Name of the property</param>
+        public PropertyPathHelper(Guid objectClass, string propertyName)
+        {
+            _oObjectClass = objectClass;
+            _sPropertyName = propertyName;
+        }
+
+        public override string ToString()
+        {
+            return "$Context/Property[Type='" + _oObjectClass.ToString() + "']/" + _sPropertyName + "$";
+        }
+    }
+}
