97f0238d47b64ad33fd690e4c9f045007603a214
JohnCardenas <Cardenas.John.J@gmail.com>
2015-08-06 18:17:52 -0700

    Clean-up, convenience and helper methods

--- CiresonPortalAPI/HardwareAsset.cs
+++ CiresonPortalAPI/HardwareAsset.cs
@@ -11,40 +11,128 @@ namespace CiresonPortalAPI
 {
     public static partial class HardwareAssetController
     {
-        public static async Task<List<HardwareAsset>> GetHardwareAssetsByCriteria(AuthorizationToken authToken, QueryCriteria criteria)
+        #region Finders
+
+        /// <summary>
+        /// Retrieves a list of HardwareAssets that match the given criteria
+        /// </summary>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="criteria">QueryCriteria to search for</param>
+        /// <param name="includeInactiveItems">If true, override the criteria to exclude inactive items (this will set the grouping operator to AND!)</param>
+        /// <returns></returns>
+        public static async Task<List<HardwareAsset>> GetHardwareAssetsByCriteria(AuthorizationToken authToken, QueryCriteria criteria, bool includeInactiveItems = false)
         {
+            if (!includeInactiveItems)
+            {
+                // Hardware Assets inherit System.ConfigItem, so we need to exclude Deleted and PendingDeletion POs
+                QueryCriteriaExpression activeItemsOnly = new QueryCriteriaExpression
+                {
+                    PropertyName = (new PropertyPathHelper(ClassConstants.HardwareAsset, "ObjectStatus")).ToString(),
+                    PropertyType = QueryCriteriaPropertyType.Property,
+                    Operator = QueryCriteriaExpressionOperator.Equal,
+                    Value = EnumerationConstants.ConfigItem.BuiltinValues.ObjectStatus.Active.ToString("B")
+                };
+
+                criteria.GroupingOperator = QueryCriteriaGroupingOperator.And;
+                criteria.Expressions.Add(activeItemsOnly);
+            }
+
             criteria.ProjectionID = TypeProjectionConstants.HardwareAsset;
             return await TypeProjectionController.GenericToSpecific<HardwareAsset>(authToken, criteria);
         }
 
         /// <summary>
-        /// Convenience method that returns a HardwareAsset by its asset tag
+        /// Convenience method that returns a list of HardwareAssets with matching asset tags
         /// </summary>
         /// <param name="authToken">AuthorizationToken to use</param>
         /// <param name="assetTag">Asset's unique asset tag</param>
         /// <returns>HardwareAsset</returns>
-        public static async Task<HardwareAsset> GetHardwareAssetByAssetTag(AuthorizationToken authToken, string assetTag)
+        public static async Task<List<HardwareAsset>> GetHardwareAssetsByAssetTag(AuthorizationToken authToken, string assetTag)
         {
-            QueryCriteriaExpression expression = new QueryCriteriaExpression();
-            expression.PropertyName = (new PropertyPathHelper(ClassConstants.HardwareAsset, "AssetTag")).ToString();
-            expression.PropertyType = QueryCriteriaPropertyType.Property;
-            expression.Operator = QueryCriteriaExpressionOperator.Equal;
-            expression.Value = assetTag;
+            QueryCriteria criteria = BuildCriteria(new PropertyPathHelper(ClassConstants.HardwareAsset, "AssetTag"), assetTag);
+            return await HardwareAssetController.GetHardwareAssetsByCriteria(authToken, criteria);
+        }
 
-            QueryCriteria criteria = new QueryCriteria(TypeProjectionConstants.HardwareAsset);
-            criteria.GroupingOperator = QueryCriteriaGroupingOperator.SimpleExpression;
-            criteria.Expressions.Add(expression);
+        /// <summary>
+        /// Convenience method that returns a HardwareAsset by its unique HardwareAssetID
+        /// </summary>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="id">HardwareAssetID</param>
+        /// <returns></returns>
+        public static async Task<HardwareAsset> GetHardwareAssetByID(AuthorizationToken authToken, Guid id)
+        {
+            QueryCriteria criteria = BuildCriteria(new PropertyPathHelper(ClassConstants.HardwareAsset, "HardwareAssetID"), id.ToString("B"));
+            List<HardwareAsset> assetList = await HardwareAssetController.GetHardwareAssetsByCriteria(authToken, criteria);
 
-            List<TypeProjection> projectionList = await TypeProjectionController.GetProjectionByCriteria(authToken, criteria);
-
-            if (projectionList.Count == 0)
+            if (assetList == null)
                 return null;
 
-            if (projectionList.Count > 1)
-                throw new Exception("More than one asset found with the same asset tag!");
+            if (assetList.Count == 0)
+                return null;
 
-            return new HardwareAsset(projectionList[0]);
+            return assetList[0];
         }
+
+        /// <summary>
+        /// Convenience method that returns a list of HardwareAssets with matching serial numbers
+        /// </summary>
+        /// <param name="authToken"></param>
+        /// <param name="serialNumber"></param>
+        /// <returns></returns>
+        public static async Task<List<HardwareAsset>> GetHardwareAssetsBySerialNumber(AuthorizationToken authToken, string serialNumber)
+        {
+            QueryCriteria criteria = BuildCriteria(new PropertyPathHelper(ClassConstants.HardwareAsset, "SerialNumber"), serialNumber);
+            return await HardwareAssetController.GetHardwareAssetsByCriteria(authToken, criteria);
+        }
+
+        #endregion Finders
+
+        #region Creators
+
+        /// <summary>
+        /// Creates a new Hardware Asset based on the supplied Template ID
+        /// </summary>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="templateId">TemplateID to use</param>
+        /// <param name="userId">ID of the user creating the Incident</param>
+        /// <returns></returns>
+        public static async Task<HardwareAsset> CreateNewHardwareAsset(AuthorizationToken authToken, Guid templateId, Guid userId)
+        {
+            TypeProjection projection = await TypeProjectionController.CreateProjectionByTemplate(authToken, templateId, userId);
+            return new HardwareAsset(projection);
+        }
+
+        #endregion Creators
+
+        #region Helpers
+
+        /// <summary>
+        /// Helper method that returns a QueryCriteria used by the GetHardwareAssetBy.. methods
+        /// </summary>
+        /// <param name="property">Property to query</param>
+        /// <param name="value">Property value to look for</param>
+        /// <returns></returns>
+        private static QueryCriteria BuildCriteria(PropertyPathHelper property, string value)
+        {
+            QueryCriteriaExpression expression = new QueryCriteriaExpression
+            {
+                PropertyName = property.ToString(),
+                PropertyType = QueryCriteriaPropertyType.Property,
+                Operator = QueryCriteriaExpressionOperator.Equal,
+                Value = value
+            };
+
+            QueryCriteria criteria = new QueryCriteria(TypeProjectionConstants.HardwareAsset)
+            {
+                GroupingOperator = QueryCriteriaGroupingOperator.SimpleExpression
+            };
+
+            criteria.Expressions.Add(expression);
+
+            return criteria;
+        }
+
+        #endregion Helpers
     }
 
 
