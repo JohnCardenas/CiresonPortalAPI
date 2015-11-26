c0b6c229b0038fb2ebcaa5e56b8f0f8661605706
JohnCardenas <Cardenas.John.J@gmail.com>
2015-11-25 18:29:37 -0800

    Rename CiresonApiException.cs to CiresonException.cs, add more exception classes

---
+++ CiresonPortalAPI/CiresonException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiresonPortalAPI
+{
+    /// <summary>
+    /// Base exception class for all CiresonPortalAPI exceptions
+    /// </summary>
+    public class CiresonException : Exception
+    {
+        public CiresonException() { }
+        public CiresonException(string message) : base(message) { }
+        public CiresonException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    /// <summary>
+    /// This exception class is thrown when an error occurs from within a Cireson REST request
+    /// </summary>
+    public class CiresonApiException : CiresonException
+    {
+        public CiresonApiException() { }
+        public CiresonApiException(string message) : base(message) { }
+        public CiresonApiException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    /// <summary>
+    /// This exception class is thrown when trying to write to a read-only property (TypeProjection and derived classes)
+    /// </summary>
+    public class CiresonReadOnlyException : CiresonException
+    {
+        public CiresonReadOnlyException() { }
+        public CiresonReadOnlyException(string message) : base(message) { }
+        public CiresonReadOnlyException(string message, Exception inner) : base(message, inner) { }
+    }
+}
