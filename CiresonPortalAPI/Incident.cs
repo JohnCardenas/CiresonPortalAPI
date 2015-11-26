c3db84a929ba343fe3c67e860f04084cbb17861c
JohnCardenas <Cardenas.John.J@gmail.com>
2015-11-25 18:32:43 -0800

    Alter Incident to comply with new TypeProjection object model

--- CiresonPortalAPI/Incident.cs
+++ CiresonPortalAPI/Incident.cs
@@ -21,7 +21,7 @@ namespace CiresonPortalAPI
         public static async Task<List<Incident>> GetIncidentsByCriteria(AuthorizationToken authToken, QueryCriteria criteria)
         {
             criteria.ProjectionID = TypeProjectionConstants.Incident;
-            return await TypeProjectionController.GenericToSpecific<Incident>(authToken, criteria);
+            return await TypeProjectionController.GetProjectionByCriteria<Incident>(authToken, criteria);
         }
 
         /// <summary>
@@ -59,225 +59,164 @@ namespace CiresonPortalAPI
         /// <returns></returns>
         public static async Task<Incident> CreateNewIncident(AuthorizationToken authToken, Guid templateId, Guid userId)
         {
-            TypeProjection projection = await TypeProjectionController.CreateProjectionByTemplate(authToken, templateId, userId);
-            return new Incident(projection);
+            TypeProjection projection = await TypeProjectionController.CreateProjectionByTemplate<Incident>(authToken, templateId, userId);
+            return (Incident)projection;
         }
     }
 
-    public class Incident : TypeProjection
+    public class Incident : WorkItem
     {
-        private User _oAffectedUser   = null;
-        private User _oAssignedToUser = null;
-
         #region Read-Only Properties
-
         /// <summary>Gets the date the Incident was closed. Read only.</summary>
-        public Nullable<DateTime> ClosedDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "ClosedDate"); } }
+        public DateTime? ClosedDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("ClosedDate"); }
+        }
 
         /// <summary>Gets the date the Incident was created. Read only.</summary>
-        public Nullable<DateTime> CreatedDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "CreatedDate"); } }
-
-        /// <summary>Gets the DisplayName of the Incident. Read only.</summary>
-        public string DisplayName { get { return DynamicObjectHelpers.GetProperty<string>(_oCurrentObject, "DisplayName"); } }
+        public DateTime? CreatedDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("CreatedDate"); }
+        }
 
         /// <summary>Gets the date the Incident was first assigned. Read only.</summary>
-        public Nullable<DateTime> FirstAssignedDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "FirstAssignedDate"); } }
+        public DateTime? FirstAssignedDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("FirstAssignedDate"); }
+        }
 
         /// <summary>Gets the date the Incident was first responded to. Read only.</summary>
-        public Nullable<DateTime> FirstResponseDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "FirstResponseDate"); } }
+        public DateTime? FirstResponseDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("FirstResponseDate"); }
+        }
 
         /// <summary>Gets the ID of the Incident. Read only.</summary>
-        public string Id { get { return DynamicObjectHelpers.GetProperty<string>(_oCurrentObject, "Id"); } }
+        public string Id
+        {
+            get { return this.GetPrimitiveValue<string>("Id"); }
+        }
 
         /// <summary>Gets the date the Incident was last modified. Read only.</summary>
-        public Nullable<DateTime> LastModifiedDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "LastModified"); } }
+        public DateTime? LastModifiedDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("LastModified"); }
+        }
 
         /// <summary>Gets the priority of this Incident. Read only.</summary>
-        public int Priority { get { return DynamicObjectHelpers.GetProperty<int>(_oCurrentObject, "Priority"); } }
+        public int Priority
+        {
+            get { return this.GetPrimitiveValue<int>("Priority"); }
+        }
 
         /// <summary>Gets the date the Incident was resolved. Read only.</summary>
-        public Nullable<DateTime> ResolvedDate { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "ResolvedDate"); } }
+        public DateTime? ResolvedDate
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("ResolvedDate"); }
+        }
 
         /// <summary>Gets the Incident's target resolution time. Read only.</summary>
-        public Nullable<DateTime> TargetResolutionTime { get { return DynamicObjectHelpers.GetProperty<Nullable<DateTime>>(_oCurrentObject, "TargetResolutionTime"); } }
-
+        public DateTime? TargetResolutionTime
+        {
+            get { return this.GetPrimitiveValue<DateTime?>("TargetResolutionTime"); }
+        }
         #endregion Read-Only Properties
 
         #region Read-Write Properties
-
         /// <summary>Gets or sets the Incident's description.</summary>
-        public string Description { get { return DynamicObjectHelpers.GetProperty<string>(_oCurrentObject, "Description"); } set { _oCurrentObject.Description = value; SetDirtyBit(); } }
+        public string Description
+        {
+            get { return this.GetPrimitiveValue<string>("Description"); }
+            set { this.SetPrimitiveValue<string>("Description", value); }
+        }
 
         /// <summary>Gets or sets the Incident's classification.</summary>
         public Enumeration Classification
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.Classification.Id, _oCurrentObject.Classification.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.Classification, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("Classification"); }
+            set { this.SetEnumeration("Classification", value); }
         }
 
         /// <summary>Gets or sets the Incident's escalation status.</summary>
-        public bool Escalated { get { return DynamicObjectHelpers.GetProperty<bool>(_oCurrentObject, "Escalated"); } set { _oCurrentObject.Escalated = value; SetDirtyBit(); } }
+        public bool Escalated
+        {
+            get { return this.GetPrimitiveValue<bool>("Escalated"); }
+            set { this.SetPrimitiveValue("Escalated", value); }
+        }
 
         /// <summary>Gets or sets the Incident's impact.</summary>
         public Enumeration Impact
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.Impact.Id, _oCurrentObject.Impact.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.Impact, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("Impact"); }
+            set { this.SetEnumeration("Impact", value); }
         }
 
         /// <summary>Gets or sets the Incident's resolution category.</summary>
         public Enumeration ResolutionCategory
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.ResolutionCategory.Id, _oCurrentObject.ResolutionCategory.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.ResolutionCategory, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("ResolutionCategory"); }
+            set { this.SetEnumeration("ResolutionCategory", value); }
         }
 
         /// <summary>Gets or sets the Incident's resolution description.</summary>
-        public string ResolutionDescription { get { return DynamicObjectHelpers.GetProperty<string>(_oCurrentObject, "ResolutionDescription"); } set { _oCurrentObject.ResolutionDescription = value; SetDirtyBit(); } }
+        public string ResolutionDescription
+        {
+            get { return this.GetPrimitiveValue<string>("ResolutionDescription"); }
+            set { this.SetPrimitiveValue<string>("ResolutionDescription", value); }
+        }
 
         /// <summary>Gets or sets the Incident's source.</summary>
         public Enumeration Source
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.Source.Id, _oCurrentObject.Source.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.Source, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("Source"); }
+            set { this.SetEnumeration("Source", value); }
         }
 
         /// <summary>Gets or sets the Incident's status.</summary>
         public Enumeration Status
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.Status.Id, _oCurrentObject.Status.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.Status, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("Status"); }
+            set { this.SetEnumeration("Status", value); }
         }
 
         /// <summary>Gets or sets the Incident's support group.</summary>
         public Enumeration SupportGroup
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.TierQueue.Id, _oCurrentObject.TierQueue.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.TierQueue, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("SupportGroup"); }
+            set { this.SetEnumeration("SupportGroup", value); }
         }
 
         /// <summary>Gets or sets the Incident's title.</summary>
-        public string Title { get { return DynamicObjectHelpers.GetProperty<string>(_oCurrentObject, "Title"); } set { _oCurrentObject.Title = value; SetDirtyBit(); } }
+        public string Title
+        {
+            get { return this.GetPrimitiveValue<string>("Title"); }
+            set { this.SetPrimitiveValue<string>("Title", value); }
+        }
 
         /// <summary>Gets or sets the Incident's urgency.</summary>
         public Enumeration Urgency
         {
-            get
-            {
-                return DeserializeEnumeration(_oCurrentObject.Urgency.Id, _oCurrentObject.Urgency.Name);
-            }
-            set
-            {
-                SetEnumerationValue(_oCurrentObject.Urgency, value);
-                SetDirtyBit();
-            }
+            get { return this.GetEnumeration("Urgency"); }
+            set { this.SetEnumeration("Urgency", value); }
         }
-
         #endregion Read-Write Properties
 
         #region Relationship Properties
 
         /// <summary>
-        /// Returns the properties of the Affected User. Read only.
+        /// Gets or sets the affected user.
         /// </summary>
-        public User AffectedUser { get { return _oAffectedUser; } set { SetAffectedUser(value); } }
+        public User AffectedUser
+        {
+            get { return this.GetRelatedObject<User>("RequestedWorkItem"); }
+            set { this.SetRelatedObject("RequestedWorkItem", value, "AffectedUser"); }
+        }
 
-        public User AssignedToUser { get { return _oAssignedToUser; } set { SetAssignedToUser(value); } }
-
+        public User AssignedToUser
+        {
+            get { return this.GetRelatedObject<User>("AssignedWorkItem"); }
+            set { this.SetRelatedObject("AssignedWorkItem", value, "AssignedToUser"); }
+        }
         #endregion Relationship Properties
-
-        #region Relationship Setters
-
-        /// <summary>
-        /// Sets the Affected User of this Incident.
-        /// </summary>
-        /// <param name="user">User to set as the Affected User</param>
-        private void SetAffectedUser(User user)
-        {
-            // Set the new Affected User
-            _oCurrentObject.RequestedWorkItem = user._oUserObj;
-            _oAffectedUser = new User(_oCurrentObject.RequestedWorkItem);
-            SetDirtyBit();
-        }
-
-        /// <summary>
-        /// Sets the Assigned To User of this Incident.
-        /// </summary>
-        /// <param name="user"></param>
-        private void SetAssignedToUser(User user)
-        {
-            // Set the new Assigned To User
-            _oCurrentObject.AssignedWorkItem = user._oUserObj;
-            _oAssignedToUser = new User(_oCurrentObject.AssignedWorkItem);
-            SetDirtyBit();
-        }
-
-        #endregion Relationship Setters
-
-        #region Constructors
-
-        /// <summary>
-        /// Constructor used internally when an existing object has been queried
-        /// </summary>
-        /// <param name="projection">Parent type projection</param>
-        internal Incident(TypeProjection projection)
-        {
-            _oOriginalObject = projection._oOriginalObject;
-            _oCurrentObject = projection._oCurrentObject;
-            _bReadOnly = false;
-
-            // Related objects
-            if (DynamicObjectHelpers.HasProperty(_oCurrentObject, "RequestedWorkItem"))
-                _oAffectedUser = new User(_oCurrentObject.RequestedWorkItem);
-        }
-
-        #endregion Constructors
-
-        public override string ToString()
-        {
-            return this.DisplayName;
-        }
     }
 }
