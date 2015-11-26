54e51130fd58a4b5395d892079cb1f93c318f3c9
JohnCardenas <Cardenas.John.J@gmail.com>
2015-11-25 18:34:46 -0800

    Add new Location and LocationController classes

---
+++ CiresonPortalAPI/Location.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+using System.Dynamic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CiresonPortalAPI
+{
+    public static partial class LocationController
+    {
+        /// <summary>
+        /// Gets a list of Locations based on the supplied criteria
+        /// </summary>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="criteria">QueryCriteria to search for</param>
+        /// <returns></returns>
+        public static async Task<List<Location>> GetLocationsByCriteria(AuthorizationToken authToken, QueryCriteria criteria)
+        {
+            criteria.ProjectionID = TypeProjectionConstants.PurchaseOrder;
+            return await TypeProjectionController.GetProjectionByCriteria<Location>(authToken, criteria);
+        }
+    }
+
+    public class Location : ConfigurationItem
+    {
+        #region Read-Write Properties
+
+        /// <summary>
+        /// Gets or sets Address Line 1 of the Location.
+        /// </summary>
+        public string AddressLine1
+        {
+            get { return this.GetPrimitiveValue<string>("LocationAddress1"); }
+            set { this.SetPrimitiveValue<string>("LocationAddress1", value, "AddressLine1"); }
+        }
+
+        /// <summary>
+        /// Gets or sets Address Line 2 of the Location.
+        /// </summary>
+        public string AddressLine2
+        {
+            get { return this.GetPrimitiveValue<string>("LocationAddress2"); }
+            set { this.SetPrimitiveValue<string>("LocationAddress2", value, "AddressLine2"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the City of the Location.
+        /// </summary>
+        public string City
+        {
+            get { return this.GetPrimitiveValue<string>("City"); }
+            set { this.SetPrimitiveValue<string>("LocationCity", value, "City"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the State of the Location.
+        /// </summary>
+        public string State
+        {
+            get { return this.GetPrimitiveValue<string>("LocationState"); }
+            set { this.SetPrimitiveValue<string>("LocationState", value, "State"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the postal (zip) code of the Location.
+        /// </summary>
+        public string PostalCode
+        {
+            get { return this.GetPrimitiveValue<string>("LocationPostCode"); }
+            set { this.SetPrimitiveValue<string>("LocationPostCode", value, "PostalCode"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the country of the Location.
+        /// </summary>
+        public string Country
+        {
+            get { return this.GetPrimitiveValue<string>("LocationCountry"); }
+            set { this.SetPrimitiveValue<string>("LocationCountry", value, "Country"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the contact name of the Location.
+        /// </summary>
+        public string ContactName
+        {
+            get { return this.GetPrimitiveValue<string>("LocationContact"); }
+            set { this.SetPrimitiveValue<string>("LocationContact", value, "ContactName"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the contact email of the Location.
+        /// </summary>
+        public string ContactEmail
+        {
+            get { return this.GetPrimitiveValue<string>("LocationEmail"); }
+            set { this.SetPrimitiveValue<string>("LocationEmail", value, "ContactEmail"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the contact phone number of the Location.
+        /// </summary>
+        public string ContactPhone
+        {
+            get { return this.GetPrimitiveValue<string>("LocationPhone"); }
+            set { this.SetPrimitiveValue<string>("LocationPhone", value, "ContactPhone"); }
+
+        }
+
+        /// <summary>
+        /// Gets or sets the contact fax number of the Location.
+        /// </summary>
+        public string ContactFax
+        {
+            get { return this.GetPrimitiveValue<string>("LocationFax"); }
+            set { this.SetPrimitiveValue<string>("LocationFax", value, "ContactFax"); }
+        }
+        #endregion Read-Write Properties
+
+        #region Relationship Properties
+
+        /// <summary>
+        /// Returns a list of all child Locations. Read only.
+        /// </summary>
+        public List<Location> Children
+        {
+            get { return this.GetRelatedObjectsList<Location>("Source_LocationContainsLocation"); }
+        }
+
+        /// <summary>
+        /// Gets or sets the parent Location.
+        /// </summary>
+        public Location Parent
+        {
+            get { return this.GetRelatedObject<Location>("Target_LocationContainsLocation"); }
+            set { this.SetRelatedObject("Target_LocationContainsLocation", value, "Parent"); }
+        }
+        #endregion Relationship Properties
+    }
+}
