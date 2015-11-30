18d9c1cdfd58d50cb35a5cbcbce2b3845dbb8d74
JohnCardenas <Cardenas.John.J@gmail.com>
2015-11-30 11:38:27 -0800

    Implement helper to retrieve active ConfigurationItems

--- CiresonPortalAPI/ConfigurationItems/ConfigurationItem.cs
+++ CiresonPortalAPI/ConfigurationItems/ConfigurationItem.cs
@@ -40,6 +40,45 @@ namespace CiresonPortalAPI.ConfigurationItems
         {
             return this.DisplayName;
         }
+
+        /// <summary>
+        /// Helper method to retrieve a list of objects derived from the ConfigurationItem class, using the specified criteria
+        /// </summary>
+        /// <typeparam name="T">ConfigurationItem derived type</typeparam>
+        /// <param name="authToken">AuthorizationToken to use</param>
+        /// <param name="criteria">QueryCriteria to use</param>
+        /// <param name="includeInactiveItems">If true, include inactive (deleted) configuration items</param>
+        /// <returns></returns>
+        internal static async Task<List<T>> GetConfigurationItemsByCriteria<T>(AuthorizationToken authToken, QueryCriteria criteria, bool includeInactiveItems = false) where T : ConfigurationItem
+        {
+            if (!includeInactiveItems)
+            {
+                PropertyPathHelper pathHelper = new PropertyPathHelper();
+                pathHelper.PropertyName = "ObjectStatus";
+
+                if (typeof(T) == typeof(HardwareAsset))
+                    pathHelper.ObjectClass = ClassConstants.HardwareAsset;
+                else if (typeof(T) == typeof(Location))
+                    pathHelper.ObjectClass = ClassConstants.Location;
+                else if (typeof(T) == typeof(PurchaseOrder))
+                    pathHelper.ObjectClass = ClassConstants.PurchaseOrder;
+                else
+                    throw new CiresonApiException("Unrecognized type " + typeof(T).FullName);
+
+                QueryCriteriaExpression activeItemsOnly = new QueryCriteriaExpression
+                {
+                    PropertyName = pathHelper.ToString(),
+                    PropertyType = QueryCriteriaPropertyType.Property,
+                    Operator = QueryCriteriaExpressionOperator.Equal,
+                    Value = EnumerationConstants.ConfigItem.BuiltinValues.ObjectStatus.Active.ToString("B")
+                };
+
+                criteria.GroupingOperator = QueryCriteriaGroupingOperator.And;
+                criteria.Expressions.Add(activeItemsOnly);
+            }
+
+            return await TypeProjectionController.GetProjectionByCriteria<T>(authToken, criteria);
+        }
         #endregion // General Methods
     }
 }
