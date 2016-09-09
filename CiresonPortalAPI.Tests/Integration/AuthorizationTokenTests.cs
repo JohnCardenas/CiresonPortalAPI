37d709b2b7d9d7e707172bc72df94a88ca6d66bb
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-08 18:03:13 -0700

    Organize AuthorizationTokenTests to allow sequential testing

--- CiresonPortalAPI.Tests/Integration/AuthorizationTokenTests.cs
+++ CiresonPortalAPI.Tests/Integration/AuthorizationTokenTests.cs
@@ -15,63 +15,44 @@ namespace CiresonPortalAPI.Tests.Integration
     public class AuthorizationTokenTests
     {
         #region Fields
-        private string _portalUrl;
-        private string _userName;
-        private string _domain;
-        private SecureString _password;
+        private static AuthorizationToken _authToken;
         #endregion // Fields
 
         #region Constructor
-        public AuthorizationTokenTests()
-        {
-            _portalUrl = ConfigurationManager.AppSettings["PortalUrl"];
-            _userName = ConfigurationManager.AppSettings["UserName"];
-            _domain = ConfigurationManager.AppSettings["Domain"];
-            _password = new SecureString();
-
-            foreach (char c in ConfigurationManager.AppSettings["Password"])
-            {
-                _password.AppendChar(c);
-            }
-
-            _password.MakeReadOnly();
-        }
+        public AuthorizationTokenTests() { }
         #endregion // Constructor
 
         [TestMethod]
-        [TestCategory("Integration")]
-        public async Task GetAuthenticatonTokenTest()
+        [TestCategory("Integration - AuthorizationController")]
+        public async Task GetAuthorizationTokenTest()
         {
             // Arrange
-            AuthorizationToken authToken;
 
             // Act
-            authToken = await AuthorizationController.GetAuthorizationToken(_portalUrl, _userName, _password, _domain);
+            _authToken = await AuthorizationController.GetAuthorizationToken(ConfigurationHelper.PortalUrl, ConfigurationHelper.UserName, ConfigurationHelper.Password, ConfigurationHelper.Domain);
 
             // Assert
-            Assert.IsNotNull(authToken);
-            Assert.AreEqual(_userName, authToken.UserName);
-            Assert.AreEqual(_domain, authToken.Domain);
-            Assert.AreEqual(_portalUrl, authToken.PortalUrl);
-            Assert.IsTrue(authToken.IsValid);
+            Assert.IsNotNull(_authToken);
+            Assert.AreEqual(ConfigurationHelper.UserName, _authToken.UserName);
+            Assert.AreEqual(ConfigurationHelper.Domain, _authToken.Domain);
+            Assert.AreEqual(ConfigurationHelper.PortalUrl, _authToken.PortalUrl);
+            Assert.IsTrue(_authToken.IsValid);
         }
 
         [TestMethod]
-        [TestCategory("Integration")]
+        [TestCategory("Integration - AuthorizationController")]
         public async Task GetUserRightsTest()
         {
             // Arrange
-            AuthorizationToken authToken;
             ConsoleUser userRights;
 
             // Act
-            authToken = await AuthorizationController.GetAuthorizationToken(_portalUrl, _userName, _password, _domain);
-            userRights = await AuthorizationController.GetUserRights(authToken);
+            userRights = await AuthorizationController.GetUserRights(_authToken);
 
             // Assert
             Assert.IsNotNull(userRights);
-            Assert.AreEqual(_userName, userRights.UserName);
-            Assert.AreEqual(_domain, userRights.Domain);
+            Assert.AreEqual(ConfigurationHelper.UserName, userRights.UserName);
+            Assert.AreEqual(ConfigurationHelper.Domain, userRights.Domain);
             Assert.IsFalse(string.IsNullOrWhiteSpace(userRights.PrincipalName));
         }
     }
