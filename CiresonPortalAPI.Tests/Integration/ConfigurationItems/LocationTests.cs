fc00af8af8581f6387b89488adda6a13d0c7e99b
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-09 14:40:35 -0700

    Add property and relationship tests

--- CiresonPortalAPI.Tests/Integration/ConfigurationItems/LocationTests.cs
+++ CiresonPortalAPI.Tests/Integration/ConfigurationItems/LocationTests.cs
@@ -15,6 +15,7 @@ namespace CiresonPortalAPI.Tests.Integration.ConfigurationItems
     {
         #region Fields
         private static AuthorizationToken _authToken;
+        private static Location _locObject;
 
         private TestContext _testContextInstance;
         #endregion // Fields
@@ -66,6 +67,72 @@ namespace CiresonPortalAPI.Tests.Integration.ConfigurationItems
             // Assert
             Assert.IsNotNull(locationList);
             Assert.IsTrue(locationList.Count >= 1);
+
+            // Save
+            _locObject = locationList[0];
+        }
+        #endregion
+
+        #region LOC02_LocationPropertiesTest
+        [TestMethod]
+        [TestCategory("Integration - Location")]
+        [Description("Tests exposing data from the underlying data model as properties")]
+        public void LOC02_LocationPropertiesTest()
+        {
+            // Arrange
+            string strData;
+            Guid guidData;
+            Enumeration enumData;
+
+            // Act
+            try
+            {
+                strData = _locObject.AddressLine1;
+                strData = _locObject.AddressLine2;
+                strData = _locObject.City;
+                strData = _locObject.ContactEmail;
+                strData = _locObject.ContactFax;
+                strData = _locObject.ContactName;
+                strData = _locObject.ContactPhone;
+                strData = _locObject.Country;
+                strData = _locObject.DisplayName;
+                strData = _locObject.Notes;
+                strData = _locObject.PostalCode;
+                strData = _locObject.State;
+
+                guidData = _locObject.Id;
+
+                enumData = _locObject.ObjectStatus;
+            }
+            // Assert
+            catch (Exception e)
+            {
+                Assert.Fail("Expected no exception, got " + e.Message);
+            }
+        }
+        #endregion
+
+        #region LOC03_LocationRelatedObjectsTest
+        [TestMethod]
+        [TestCategory("Integration - Location")]
+        [Description("Tests retrieving related objects")]
+        public void LOC03_LocationRelatedObjectsTest()
+        {
+            // Arrange
+            Location parent;
+            List<Location> children;
+
+            // Act
+            try
+            {
+                parent = _locObject.Parent;
+                children = _locObject.Children;
+            }
+            // Assert
+            catch (Exception e)
+            {
+                Assert.Fail("Expected no exception, got " + e.Message);
+            }
         }
         #endregion
     }
