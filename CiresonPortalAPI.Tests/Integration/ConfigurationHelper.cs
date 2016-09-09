41596c54ea7d620243055b0d8dd89ba88a82fcc3
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-08 18:04:56 -0700

    Implement ConfigurationHelper and UserTests

---
+++ CiresonPortalAPI.Tests/Integration/ConfigurationHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+using System.Security;
+
+namespace CiresonPortalAPI.Tests.Integration
+{
+    public static class ConfigurationHelper
+    {
+        public static string UserName
+        {
+            get { return ConfigurationManager.AppSettings["UserName"]; }
+        }
+
+        public static SecureString Password
+        {
+            get
+            {
+                SecureString pwd = new SecureString();
+
+                foreach (char c in ConfigurationManager.AppSettings["Password"])
+                {
+                    pwd.AppendChar(c);
+                }
+
+                pwd.MakeReadOnly();
+                return pwd;
+            }
+        }
+
+        public static string Domain
+        {
+            get { return ConfigurationManager.AppSettings["Domain"]; }
+        }
+
+        public static string PortalUrl
+        {
+            get { return ConfigurationManager.AppSettings["PortalUrl"]; }
+        }
+    }
+}
---
+++ CiresonPortalAPI.Tests/Integration/ConfigurationItems/UserTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CiresonPortalAPI.ConfigurationItems;
+
+namespace CiresonPortalAPI.Tests.Integration.ConfigurationItems
+{
+    /// <summary>
+    /// Summary description for UserTests
+    /// </summary>
+    [TestClass]
+    public class UserTests
+    {
+        #region Fields
+        public static AuthorizationToken _authToken;
+        public static Guid _userId;
+
+        private TestContext _testContextInstance;
+        #endregion // Fields
+
+        #region Constructor
+        public UserTests() { }
+        #endregion // Constructor
+
+        #region Properties
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get { return _testContextInstance;  }
+            set { _testContextInstance = value; }
+        }
+        #endregion // Properties
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            Task<AuthorizationToken> tokenTask = AuthorizationController.GetAuthorizationToken(ConfigurationHelper.PortalUrl, ConfigurationHelper.UserName, ConfigurationHelper.Password, ConfigurationHelper.Domain);
+            tokenTask.Wait();
+            _authToken = tokenTask.Result;
+        }
+
+        [TestMethod]
+        [TestCategory("Integration - UserController")]
+        public async Task GetUserListTest()
+        {
+            // Arrange
+            const int MAX_USERS = 5; // Set this low to make sure the basic get list function works
+            List<User> userList;
+
+            // Act
+            userList = await UserController.GetUserList(_authToken, "", false, false, MAX_USERS, true);
+
+            // Assert
+            Assert.AreEqual(MAX_USERS, userList.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Integration - UserController")]
+        public async Task GetAnalystUserListTest()
+        {
+            // Arrange
+            List<User> userList;
+
+            // Act
+            userList = await UserController.GetUserList(_authToken, "", true, false, 1);
+
+            // Assert
+            Assert.AreEqual(1, userList.Count);
+            Assert.IsNotNull(userList[0]);
+
+            // Setup for next test
+            _userId = userList[0].Id;
+        }
+
+        [TestMethod]
+        [TestCategory("Integration - UserController")]
+        public async Task GetUserByIdTest()
+        {
+            // Arrange
+            User user;
+
+            // Act
+            user = await UserController.GetUserById(_authToken, _userId);
+
+            // Assert
+            Assert.IsNotNull(user);
+        }
+    }
+}
