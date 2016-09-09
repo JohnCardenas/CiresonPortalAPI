fa65020a7d08f1bb0333683a27f6674195d98b4a
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-09 11:24:50 -0700

    Implement location test

---
+++ CiresonPortalAPI.Tests/Integration/ConfigurationItems/LocationTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CiresonPortalAPI.ConfigurationItems;
+
+namespace CiresonPortalAPI.Tests.Integration.ConfigurationItems
+{
+    /// <summary>
+    /// Summary description for LocationTests
+    /// </summary>
+    [TestClass]
+    public class LocationTests
+    {
+        #region Fields
+        private static AuthorizationToken _authToken;
+
+        private TestContext _testContextInstance;
+        #endregion // Fields
+
+        #region Constructor
+        public LocationTests() { }
+        #endregion // Constructor
+
+        #region Properties
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return _testContextInstance;
+            }
+            set
+            {
+                _testContextInstance = value;
+            }
+        }
+        #endregion // Properties
+
+        #region Class Initializer
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            Task<AuthorizationToken> tokenTask = AuthorizationController.GetAuthorizationToken(ConfigurationHelper.PortalUrl, ConfigurationHelper.UserName, ConfigurationHelper.Password, ConfigurationHelper.Domain);
+            tokenTask.Wait();
+            _authToken = tokenTask.Result;
+        }
+        #endregion // Class Initializer
+
+        #region LOC01_GetAllLocationsTest
+        [TestMethod]
+        [TestCategory("Integration - LocationController")]
+        [Description("Tests a fetch of all location objects")]
+        public async Task LOC01_GetAllLocationsTest()
+        {
+            // Arrange
+            List<Location> locationList;
+
+            // Act
+            locationList = await LocationController.GetAllLocations(_authToken);
+
+            // Assert
+            Assert.IsNotNull(locationList);
+            Assert.IsTrue(locationList.Count >= 1);
+        }
+        #endregion
+    }
+}
