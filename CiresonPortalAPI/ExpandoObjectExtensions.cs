6e17b769fa12047bf7c84966390b7ef68a9747f6
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-13 15:07:02 -0700

    Move ExpandoObject deep copy into extension method

---
+++ CiresonPortalAPI/ExpandoObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CiresonPortalAPI
+{
+    internal static class ExpandoObjectExtensions
+    {
+        /// <summary>
+        /// Deep copies an ExpandoObject though serialization
+        /// </summary>
+        /// <param name="obj">ExpandoObject to copy</param>
+        /// <returns></returns>
+        public static ExpandoObject DeepCopy(this ExpandoObject obj)
+        {
+            string data = JsonConvert.SerializeObject(obj);
+            ExpandoObjectConverter converter = new ExpandoObjectConverter();
+
+            return JsonConvert.DeserializeObject<ExpandoObject>(data, converter);
+        }
+    }
+}
--- CiresonPortalAPI/TypeProjectionController.cs
+++ CiresonPortalAPI/TypeProjectionController.cs
@@ -149,7 +149,7 @@ namespace CiresonPortalAPI
                     CultureInfo culture = null;
                     T instanceType = (T)Activator.CreateInstance(typeof(T), flags, null, null, culture);
 
-                    instanceType.OriginalObject = DeepCopyExpando(obj);
+                    instanceType.OriginalObject = obj.DeepCopy();
                     instanceType.CurrentObject = obj;
                     instanceType.ReadOnly = false;
 
@@ -162,19 +162,6 @@ namespace CiresonPortalAPI
             {
                 throw; // Rethrow exceptions
             }
-        }
-
-        /// <summary>
-        /// Deep copies an ExpandoObject though serialization
-        /// </summary>
-        /// <param name="obj">ExpandoObject to copy</param>
-        /// <returns></returns>
-        private static ExpandoObject DeepCopyExpando(ExpandoObject obj)
-        {
-            string data = JsonConvert.SerializeObject(obj);
-            ExpandoObjectConverter converter = new ExpandoObjectConverter();
-
-            return JsonConvert.DeserializeObject<ExpandoObject>(data, converter);
         }
     }
 }
