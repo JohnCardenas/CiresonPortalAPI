8a1be402a0495f760cf9e669103a28294e43a7c5
JohnCardenas <Cardenas.John.J@gmail.com>
2016-09-13 11:01:28 -0700

    Add Location and PurchaseOrder default template IDs

--- CiresonPortalAPI/TemplateConstants.cs
+++ CiresonPortalAPI/TemplateConstants.cs
@@ -21,18 +21,6 @@ namespace CiresonPortalAPI
         }
 
         /// <summary>
-        /// Built-in templates for Service Requests
-        /// </summary>
-        public static class ServiceRequest
-        {
-            /// <summary>
-            /// Default Service Request (ServiceManager.ServiceRequest.Library.Template.DefaultServiceRequest)
-            /// Default Service Request
-            /// </summary>
-            public static Guid Default { get { return new Guid("{03bc9162-041f-c987-8ce4-a5547cd9ca04}"); } }
-        }
-
-        /// <summary>
         /// Built-in templates for Cireson Hardware Assets
         /// </summary>
         public static class HardwareAsset
@@ -42,6 +30,34 @@ namespace CiresonPortalAPI
             /// Cireson Asset Management Hardware Asset Tempate (Portal)
             /// </summary>
             public static Guid Default { get { return new Guid("5b20b20f-0e6d-556b-8247-9b53e2393b73"); } }
+        }
+
+        /// <summary>
+        /// Built-in templates for Cireson Locations
+        /// </summary>
+        public static class Location
+        {
+            public static Guid Default { get { return new Guid("{a6b537ca-d0cf-b0b9-3a3c-40e96d4c8ba0}"); } }
+        }
+
+        /// <summary>
+        /// Built-in templates for Cireson Purchase Orders
+        /// </summary>
+        public static class PurchaseOrder
+        {
+            public static Guid Default { get { return new Guid("{6288c684-569f-d9fa-efce-3cf0ca512e61}"); } }
+        }
+
+        /// <summary>
+        /// Built-in templates for Service Requests
+        /// </summary>
+        public static class ServiceRequest
+        {
+            /// <summary>
+            /// Default Service Request (ServiceManager.ServiceRequest.Library.Template.DefaultServiceRequest)
+            /// Default Service Request
+            /// </summary>
+            public static Guid Default { get { return new Guid("{03bc9162-041f-c987-8ce4-a5547cd9ca04}"); } }
         }
     }
 }
